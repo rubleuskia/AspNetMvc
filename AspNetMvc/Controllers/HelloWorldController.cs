8a65bb066eec172a47a3f9ce6e0396ce72370269
Aleksey Rublevsky <aliakseiru@playtika.com>
2021-06-08 16:36:37 +0300

    Add a controller

---
+++ AspNetMvc/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvc.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        public string Welcome()
+        {
+            return "This is the welcome method";
+        }
+
+        // https://localhost:{PORT}/HelloWorld/Welcome?name=Rick&numtimes=4
+        public string Welcome1(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+
+        // https://localhost:{PORT}/HelloWorld/Welcome/3?name=Rick
+        public string Welcome2(string name, int id = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {id}");
+        }
+    }
+}
