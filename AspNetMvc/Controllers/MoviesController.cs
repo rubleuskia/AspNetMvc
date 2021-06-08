30ee15ecee0b894193b17c83e39f6dbaf271f122
Aleksey Rublevsky <aliakseiru@playtika.com>
2021-06-08 16:53:00 +0300

    Add a view

---
+++ AspNetMvc/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetMvc.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
